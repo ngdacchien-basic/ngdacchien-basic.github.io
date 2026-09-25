4d562fa56d528c521185739055467e9b8d9134b2
ngdacchien-basic <icloud.ngdacchien@gmail.com>
2026-09-25 21:16:50 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'YOUR_SUPABASE_URL',       // Thay bằng URL của bạn (VD: https://xxxx.supabase.co)
+    SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY' // Thay bằng Anon/Public Key của bạn
+};
