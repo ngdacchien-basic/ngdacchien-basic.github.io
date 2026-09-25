f96095f6dbf34486dfa8a33f5dac3992b7ba861c
ngdacchien-basic <icloud.ngdacchien@gmail.com>
2026-09-25 21:19:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SUPABASE_URL: 'YOUR_SUPABASE_URL',       // Thay bằng URL của bạn (VD: https://xxxx.supabase.co)
-    SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY' // Thay bằng Anon/Public Key của bạn
+    SUPABASE_URL: 'https://wdffwpdwwbhgodxtxnpd.supabase.co',       // Thay bằng URL của bạn (VD: https://xxxx.supabase.co)
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkZmZ3cGR3d2JoZ29keHR4bnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMzkzMDUsImV4cCI6MjEwNTkxNTMwNX0.ewzVTZObm7yB4FlgPdBA7O-ZVfgNhaoyRCGr1dthsNE' // Thay bằng Anon/Public Key của bạn
 };
